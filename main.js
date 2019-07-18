7d4ce4dbd10b712f7e241d11a1c742dbe08bdd62
Cameron Bourne <CameronBourne@users.noreply.github.com>
2019-07-19 09:13:26 +1000

    I caused a merge conflict.

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-// Update: Now in 2019
-console.log("hello world");
+ -// Update: Now in 2019
+console.log("I caused a merge conflict.");
+
+//Update By Cameron - Breaking code added "I caused a merge conflict."  to log
